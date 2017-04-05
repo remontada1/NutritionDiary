0af774aa71aa75020b752b43638a716be1cf7394
remontada1 <remontada1@gmail.com>
2017-04-05 20:11:56 +0300

    add Customer model

---
+++ WebApplication1/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+
+        public int OrderId { get; set; }
+
+        
+    }
+
+}
