365cddb47323b8d2f9428730115156b4ba4f2de6
remontada1 <remontada1@gmail.com>
2017-04-05 20:40:37 +0300

    Add data to Customer list

--- WebApplication1/Controllers/CustomerController.cs
+++ WebApplication1/Controllers/CustomerController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Web.Http;
+using System.Web.Http;  
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class CustomerController : ApiController
     {
-        
-
-
+        List<Customer> customers = new List<Customer>()
+        {
+            new Customer { Id = 1, Name = "Иван", OrderId = 4, PhoneNumber = "+380634750253"},
+            new Customer { Id = 2, Name = "Den", OrderId = 3, PhoneNumber= "+380637350223"}
+        };
     }
 }
