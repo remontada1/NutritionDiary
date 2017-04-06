d8e632b4f9b2df68392608af54f57ae1d044546d
remontada1 <remontada1@gmail.com>
2017-04-06 22:14:21 +0300

    Add CustomerRepository
    Add CRUD methods to repository, add dependency injection to CustomerController

--- WebApplication1/Controllers/CustomerController.cs
+++ WebApplication1/Controllers/CustomerController.cs
@@ -10,10 +10,43 @@ namespace WebApplication1.Controllers
 {
     public class CustomerController : ApiController
     {
+        ICustomerRepository _repository;
+
+        public CustomerController(ICustomerRepository repository)
+        {
+            _repository = repository;
+        }
+
+
         List<Customer> customers = new List<Customer>()
         {
             new Customer { Id = 1, Name = "Иван", OrderId = 4, PhoneNumber = "+380634750253"},
             new Customer { Id = 2, Name = "Den", OrderId = 3, PhoneNumber= "+380637350223"}
         };
+
+
+
+        public IEnumerable<Customer> GetAll()
+        {
+            return repository.GetAll();
+        }
+
+        public Customer GetById(int id)
+        {
+            return repository.GetById(id);
+        }
+
+        public Customer Add(Customer item)
+        {
+            item = _repository.Add(item);
+            return item;
+        }
+
+        public void Remove(int id)
+        {
+            Customer customer = _repository.GetById(id);
+            _repository.Remove(id);
+        }
+        
     }
 }
---
+++ WebApplication1/Models/ICustomerRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Models
+{
+    public interface ICustomerRepository
+    {
+        IEnumerable<Customer> GetAll();
+        Customer GetById(int Id);
+        Customer Add(Customer item);
+        void Remove(int Id);
+
+    }
+}
