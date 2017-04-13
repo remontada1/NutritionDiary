489059da98a4bf1370263c85429c2bb3f439981a
remontada1 <remontada1@gmail.com>
2017-04-13 23:54:08 +0300

    Add HTTP methods to CustomerController

--- WebApplication1/Controllers/CustomerController.cs
+++ WebApplication1/Controllers/CustomerController.cs
@@ -11,29 +11,29 @@ namespace WebApplication1.Controllers
     public class CustomerController : ApiController
     {
         ICustomerRepository repository;
-
+        
         public CustomerController(ICustomerRepository repository)
         {
             this.repository = repository;
         }
 
-
+        [HttpGet]
         public IEnumerable<Customer> GetAll()
         {
             return repository.GetAll();
         }
-
+        [HttpGet]
         public Customer GetById(int id)
         {
             return repository.GetById(id);
         }
-
+        [HttpPost]
         public Customer Add(Customer item)
         {
             item = repository.Add(item);
             return item;
         }
-
+        [HttpDelete]
         public void Remove(int id)
         {
             Customer customer = repository.GetById(id);
