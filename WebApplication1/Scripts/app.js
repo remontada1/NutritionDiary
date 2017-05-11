6848363c7a86a27a61f0548bdd4a46989b241b6e
remontada1 <remontada1@gmail.com>
2017-05-11 19:27:05 +0300

    Add Index.html page Add Unity resolver

---
+++ WebApplication1/Scripts/app.js
@@ -0,0 +1,9 @@
+﻿var app = angular.module('adventureModule', []);
+app.factory('userService', function ($http) {
+    var factory = {};
+    factory.getallrecords = function () {
+        return $http.get('api/Customer/GetAll');
+    }
+    return factory;
+});
+
