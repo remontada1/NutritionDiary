4a66ec6ef09df7929dca7a4b8056c3ccdb123c30
remontada1 <remontada1@gmail.com>
2017-06-01 17:42:57 +0300

    add WebApiConfig  and Global.Asax to repo

---
+++ WebApplication1/App_Start/WebApiConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Microsoft.Practices.Unity;
+using WebApplication1.Resolver;
+using WebApplication1.Repository;
+using WebApplication1.Models;
+using System.Net.Http.Headers;
+
+
+namespace WebApplication1
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            var container = new UnityContainer();
+            container.RegisterType<ICustomerRepository, CustomerRepository>(new HierarchicalLifetimeManager());
+            config.DependencyResolver = new UnityResolver(container);
+
+            
+
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "Customer",
+                routeTemplate: "api/customer/{id}",
+                defaults: new { controller = "customer", id = RouteParameter.Optional }
+            );
+        }
+    }
+}
