bc3e66f04613e5be318d462e78b11434c0294cd1
remontada1 <remontada1@gmail.com>
2017-07-18 19:55:52 +0300

    Add ViewModelToDomainMappingProfile

---
+++ WebApplication1/Mappings/ViewModelToDomainMappingProfile .cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.ViewModels;
+using AutoMapper;
+using WebApplication1.Models;
+namespace WebApplication1.Mappings
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get { return "ViewModelToDomainMappings"; }
+        }
+
+        protected override void Configure()
+        {
+            Mapper.CreateMap<FoodViewModel, Food>()
+                .ForMember(f => f.Name, map => map.MapFrom(vm => vm.FoodName))
+                .ForMember(f => f.Hydrates, map => map.MapFrom(vm => vm.Hydrates))
+                .ForMember(f => f.KCalory, map => map.MapFrom(vm => vm.KCalory))
+                .ForMember(f => f.Fats, map => map.MapFrom(vm => vm.Fats))
+                .ForMember(f => f.Protein, map => map.MapFrom(vm => vm.Protein));
+        }
+    }
+}
