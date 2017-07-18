10e2f046024a6cc005a7a0aca7895df8cf4b914e
remontada1 <remontada1@gmail.com>
2017-07-18 19:34:52 +0300

    Add Initialization for FoodVM

---
+++ WebApplication1/Mappings/AutoMapperConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+namespace WebApplication1.Mappings
+{
+    public class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.Initialize(x =>
+                {
+                    x.AddProfile<DomainToViewModelMappingFood>();
+                    x.AddProfile<ViewModelToDomainMappingFood>();
+                });
+        }
+
+    }
+}
