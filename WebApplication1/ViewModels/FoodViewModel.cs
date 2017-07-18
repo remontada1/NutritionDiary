26983bf876445108ef2e866b9ba66c4c72e6ab76
remontada1 <remontada1@gmail.com>
2017-07-18 19:23:49 +0300

    Add FoodViewModel

---
+++ WebApplication1/ViewModels/FoodViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+using AutoMapper;
+
+namespace WebApplication1.ViewModels
+{
+    public class FoodViewModel
+    {
+        public int FoodId { get; set; }
+        public string FoodName { get; set; }
+        public int KCalory { get; set;}
+        public int Protein { get; set;}
+        public int Hydrates { get; set;}
+        public int Fats {get; set;}
+        public string Image { get; set; }
+    }
+}
