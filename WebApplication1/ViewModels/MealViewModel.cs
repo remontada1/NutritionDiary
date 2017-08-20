54072dcf7ea158d8c81e20073b47466567a8aa33
remontada1 <remontada1@gmail.com>
2017-08-20 15:47:50 +0300

    Add MealViewModel

---
+++ WebApplication1/ViewModels/MealViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.ViewModels
+{
+    public class MealViewModel
+    {
+        public string MealName { get; set; }
+        public string FoodName { get; set; }
+
+        public int TotalCalories { get; set; }
+
+        public int KCalories { get; set; }
+
+        public int Weight { get; set; }
+
+    }
+}
