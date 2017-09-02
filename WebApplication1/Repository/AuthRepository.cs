3002ad2261f898c9eed4638fc9abd21f2c35a048
remontada1 <remontada1@gmail.com>
2017-09-02 06:05:49 +0300

    Add RegisterUser method to AuthRepository

---
+++ WebApplication1/Repository/AuthRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using WebApplication1.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using WebApplication1.Models;
+using WebApplication1.DAL;
+using WebApplication1.Infrastructure;
+using System.Threading.Tasks;
+
+
+namespace WebApplication1.Repository
+{
+    public class AuthRepository : RepositoryBase<Food>
+    {
+        public AuthRepository(IDbFactory dbFactory)
+            : base(dbFactory) { }
+
+        private UserManager<IdentityUser> _userManager;
+
+        public async Task<IdentityResult> RegisterUser(User user)
+        {
+            IdentityUser identityUser = new IdentityUser
+            {
+                UserName = user.UserName
+            };
+
+            var result = await _userManager.CreateAsync(identityUser, user.PasswordHash);
+
+            return result;
+
+        }
+
+
+
+    }
+}
