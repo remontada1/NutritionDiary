fa7f04b310c81084653dcc662df7dc96b5433179
remontada1 <remontada1@gmail.com>
2017-09-02 06:21:50 +0300

    Add UserBingingModel

---
+++ WebApplication1/Models/UserBindingModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class UserBindingModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {1} charachters long.",  MinimumLength = 6)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match")]
+        public string ConfirmationPassword { get; set; }
+
+    }
+}
