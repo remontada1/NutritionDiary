b15cc69aeb84e9ee20d53b1de12434d945cbaf6e
Vitalii <vitalii.meshcheriakov@gmail.com>
2018-07-09 18:36:12 +0300

    Separate get GUID logic from MealRepisitory, add this logic to created class

---
+++ WebApplication1/Repository/GuidRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+using WebApplication1.Infrastructure;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity;
+
+namespace WebApplication1.Repository
+{
+    public class GuidRepository : RepositoryBase<User>, IGuidRepository
+    {
+        public GuidRepository(IDbFactory dbFactory)
+            : base(dbFactory)
+        { }
+        public User GetUserByGuid()
+        {
+            Guid guid = Guid.Empty;
+            var currentUserId = HttpContext.Current.User.Identity.GetUserId();
+
+            guid = new Guid(currentUserId); //convert to guid format
+
+            var user = DbContext.Users.Find(guid);
+
+            return user;
+        }
+    }
+    public interface IGuidRepository
+    {
+        User GetUserByGuid();
+    }
+
+}
