121e225dc0efa3468a56002b14be47724f488057
Vitalii <vitalii.meshcheriakov@gmail.com>
2018-08-16 18:34:32 +0300

    Add DTO for MealTotalNutrients method

---
+++ WebApplication1/Models/DateDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class DateDTO
+    {
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{dd/MM/yyyy}")]
+        public DateTime Date { get; set; }
+
+    }
+}
