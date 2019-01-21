895372c4cf3f1511cc2382249c59ae6d9a79710c
Vitalii <vitalii.meshcheriakov@gmail.com>
2019-01-22 00:03:45 +0200

    Move AddSumOfNutritionPerMeal_ShouldReturnCorrectSum to MealRepositoryTest class

--- NutritionDiary.TestProject/FoodServiceTests.cs
+++ NutritionDiary.TestProject/FoodServiceTests.cs
@@ -28,26 +28,6 @@ namespace NutritionDiary.TestProject
                 new Food { Id = 42, Fats = 13 },
                 new Food { Id = 21, Fats = 33 }
             };
-
-            mealList = new List<Meal>()
-            {
-                new Meal() {
-                    Id = 1,
-                    SetDate = new DateTime(2017, 10, 17),
-                    Foods = new List<Food>() {
-                        new Food { Id = 42, KCalory = 155, Hydrates = 30, Fats = 10, Protein = 6},
-                        new Food { Id = 21, KCalory = 55, Hydrates = 12, Fats = 5, Protein = 3 }
-                        }
-                },
-                 new Meal() {
-                    Id = 2,
-                    SetDate = new DateTime(2017, 10, 17),
-                    Foods = new List<Food>() {
-                        new Food { Id = 43, KCalory = 150, Hydrates = 30, Fats = 10, Protein = 6},
-                        new Food { Id = 22, KCalory = 55, Hydrates = 12, Fats = 5, Protein = 3 }
-                        }
-                }
-            };
         }
 
 
@@ -78,37 +58,6 @@ namespace NutritionDiary.TestProject
 
             Assert.NotNull(result);
             Assert.AreEqual(21, result.Id);
-        }
-
-        [Test]
-        public void AddSumOfNutritionPerMeal_ShouldReturnCorrectSum()
-        {
-
-            var mealRepository = new Mock<IMealRepository>();
-            var unitOfWork = new Mock<IUnitOfWork>().Object;
-
-
-            mealRepository.Setup(x => x.SumOfNutrientsPerMeal(1))
-                    .Returns(new Func<int, MealTotalNutrients>
-                    (f => mealList.GroupBy(x => x.Id).Select(x => new MealTotalNutrients
-                    {
-                        TotalCalories = x.Sum(k => k.Foods.Sum(c => (int?)c.KCalory)),
-                        TotalFats = x.Sum(k => k.Foods.Sum(c => (int?)c.Fats)),
-                        TotalCarbs = x.Sum(k => k.Foods.Sum(c => (int?)c.Hydrates)),
-                        TotalProteins = x.Sum(k => k.Foods.Sum(c => (int?)c.Protein))
-                    }).FirstOrDefault()));
-
-
-            var service = new MealService(null, mealRepository.Object, null, unitOfWork);
-
-
-            var result = service.SumOfNutrientsPerMeal(1);
-
-
-            Assert.AreEqual(42, result.TotalCarbs);
-            Assert.AreEqual(210, result.TotalCalories);
-            Assert.AreEqual(15, result.TotalFats);
-            Assert.AreEqual(9, result.TotalProteins);
         }
     }
 }
---
+++ NutritionDiary.TestProject/MealRepositoryTest.cs
@@ -0,0 +1,71 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebApplication1.Infrastructure;
+using WebApplication1.Models;
+using WebApplication1.Repository;
+using WebApplication1.Service;
+
+namespace NutritionDiary.TestProject
+{
+    class MealRepositoryTest
+    {
+        private Mock<IMealRepository> mockRepository;
+        private IUnitOfWork unitOfWork;
+        private List<Meal> mealList;
+        private MealService service;
+
+        [SetUp]
+        public void SetUp()
+        {
+            mockRepository = new Mock<IMealRepository>();
+            unitOfWork = new Mock<IUnitOfWork>().Object;
+            service = new MealService(null, mockRepository.Object, null, unitOfWork);
+
+            mealList = new List<Meal>()
+            {
+                new Meal()  {
+                    Id = 1,
+                    SetDate = new DateTime(2017, 10, 17),
+                    Foods = new List<Food>() {
+                        new Food { Id = 42, KCalory = 155, Hydrates = 30, Fats = 10, Protein = 6},
+                        new Food { Id = 21, KCalory = 55, Hydrates = 12, Fats = 5, Protein = 3 }
+                    }
+                },
+                new Meal() {
+                    Id = 2,
+                    SetDate = new DateTime(2017, 10, 17),
+                    Foods = new List<Food>() {
+                        new Food { Id = 43, KCalory = 150, Hydrates = 30, Fats = 10, Protein = 6},
+                        new Food { Id = 22, KCalory = 55, Hydrates = 12, Fats = 5, Protein = 3 }
+                    }
+                }
+            };
+        }
+
+        [Test]
+        public void AddSumOfNutritionPerMeal_ShouldReturnCorrectSum()
+        {
+
+            mockRepository.Setup(x => x.SumOfNutrientsPerMeal(1))
+                    .Returns(new Func<int, MealTotalNutrients>
+                    (f => mealList.GroupBy(x => x.Id).Select(x => new MealTotalNutrients
+                    {
+                        TotalCalories = x.Sum(k => k.Foods.Sum(c => (int?)c.KCalory)),
+                        TotalFats = x.Sum(k => k.Foods.Sum(c => (int?)c.Fats)),
+                        TotalCarbs = x.Sum(k => k.Foods.Sum(c => (int?)c.Hydrates)),
+                        TotalProteins = x.Sum(k => k.Foods.Sum(c => (int?)c.Protein))
+                    }).FirstOrDefault()));
+
+            var result = service.SumOfNutrientsPerMeal(1);
+
+
+            Assert.AreEqual(42, result.TotalCarbs);
+            Assert.AreEqual(210, result.TotalCalories);
+            Assert.AreEqual(15, result.TotalFats);
+            Assert.AreEqual(9, result.TotalProteins);
+        }
+    }
+}
